26217f4b9951282710e5ffd77c5dfd7ef00b729c
Wenping Guo <ybyygu@gmail.com>
2019-09-30 18:22:06 +0800

    define Survive trait for survivor gear

--- src/gears/survivor.rs
+++ src/gears/survivor.rs
@@ -1,5 +1,21 @@
+// imports
+
+// [[file:~/Workspace/Programming/structure-predication/spdkit/spdkit.note::*imports][imports:1]]
+use crate::common::*;
+use crate::encoding::*;
+use crate::individual::*;
+use crate::operators::*;
+use crate::population::*;
+use crate::random::*;
+
+use super::*;
+// imports:1 ends here
+
 // base
 
 // [[file:~/Workspace/Programming/structure-predication/spdkit/spdkit.note::*base][base:1]]
-
+/// Member surviving by removing bad performing individuals
+pub trait Survive<G: Genome>: Clone {
+    fn survive<R: Rng + Sized>(&mut self, population: &Population<G>, rng: &mut R) -> usize;
+}
 // base:1 ends here
