3ec4c3ea689cca480e0ccceaf5b84a45a9eec850
Wenping Guo <ybyygu@gmail.com>
2022-06-16 13:42:21 +0800

    fingerprint: use hash code as a short alias

--- src/fingerprint.rs
+++ src/fingerprint.rs
@@ -59,15 +59,26 @@ fn test_molecule_reorder() -> Result<()> {
 }
 // equivalent atoms:1 ends here
 
-// [[file:../spdkit.note::*pub][pub:1]]
+// [[file:../spdkit.note::3d0c2d80][3d0c2d80]]
 fn reorder_atoms_canonically(mol: &mut Molecule) {
     let nodes: Vec<_> = mol.numbers().collect();
     let edges: Vec<_> = mol.bonds().map(|(i, j, _)| (i, j)).collect();
     let colors: Vec<_> = mol.atomic_numbers().collect();
 
+    dbg!();
     let labels = nauty::get_canonical_labels(&nodes, &edges, &colors).expect("nauty failure");
+    dbg!();
+    assert_eq!(labels.len(), nodes.len());
     // NOTE: make permutation into sorting order. it is tricky.
-    let mapping: std::collections::HashMap<_, _> = labels.into_iter().enumerate().map(|(i, l)| (l as usize, i)).collect();
+    let mapping: std::collections::HashMap<_, _> = labels.iter().enumerate().map(|(i, l)| (*l as usize, i)).collect();
+
+    // // FIXME: remove
+    // for i in nodes.iter() {
+    //     if !mapping.contains_key(i) {
+    //         dbg!(&nodes, &labels, &edges, &colors, &mapping);
+    //         gut::utils::sleep(5.0);
+    //     }
+    // }
     let orders: Vec<_> = nodes.iter().map(|i| mapping[&i]).collect();
     mol.reorder(&orders);
 }
@@ -83,7 +94,8 @@ impl FingerPrintExt for Molecule {
     fn fingerprint(&self) -> String {
         let mut mol = self.clone();
         reorder_atoms_canonically(&mut mol);
-        crate::graph6::encode_molecule_as_graph6(&mol)
+        let fp = crate::graph6::encode_molecule_as_graph6(&mol);
+        gut::utils::hash_code(&fp)
     }
 
     /// This is an operation of reordering the atoms in a way that does not depend
@@ -93,9 +105,9 @@ impl FingerPrintExt for Molecule {
         reorder_atoms_canonically(self);
     }
 }
-// pub:1 ends here
+// 3d0c2d80 ends here
 
-// [[file:../spdkit.note::*test][test:1]]
+// [[file:../spdkit.note::fb3d7a90][fb3d7a90]]
 #[test]
 fn test_molecule_fingerprint() -> Result<()> {
     let mol = Molecule::from_file("./tests/files/H2O-rotated.mol2")?;
@@ -109,6 +121,35 @@ fn test_molecule_fingerprint() -> Result<()> {
     let fp3 = mol.fingerprint();
     assert_ne!(fp1, fp3);
 
+    let nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+    let colors = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6];
+    let edges = [
+        (1, 22),
+        (2, 22),
+        (3, 22),
+        (4, 20),
+        (5, 18),
+        (6, 19),
+        (7, 21),
+        (8, 21),
+        (9, 10),
+        (11, 16),
+        (11, 21),
+        (12, 21),
+        (12, 15),
+        (13, 18),
+        (13, 17),
+        (14, 22),
+        (14, 15),
+        (14, 17),
+        (16, 17),
+        (19, 20),
+    ];
+
+    let labels = nauty::get_canonical_labels(&nodes, &edges, &colors);
+    dbg!(labels);
+    let labels = [1, 2, 9, 10, 5, 9, 10, 8, 6, 7, 11, 18, 11, 21, 20, 22, 22, 18, 21, 20, 17, 19];
+
     Ok(())
 }
-// test:1 ends here
+// fb3d7a90 ends here
